e69fee3cfd270a1cecca7b1704484cb906cefb2d
Miguel <miguelviannafeiteira@gmail.com>
2021-07-14 11:11:53 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -3,30 +3,40 @@ export default class Modal {
     this.botaoAbrir = document.querySelector(botaoAbrir);
     this.botaoFechar = document.querySelector(botaoFechar);
     this.containerModal = document.querySelector(containerModal);
+    //  binda this ao callback paara fazer referencia ao objeto da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
   }
-  
+
+  // abre ou fecha o modal
   toggleModal() {
     this.containerModal.classList.toggle('ativo');
   }
 
+  // adiciona o evento de toggle ao modal
   eventToggleModal(event) {
     event.preventDefault();
     this.toggleModal();
   }
 
+  //  fecha o modal ao clicar do lado de fora
   cliqueForaModal(event) {
-    if (event.target === this) {
+    if (event.target === this.containerModal) {
       this.toggleModal(event);
     }
   }
 
+  //  adiciona os eventos aos elementos do modal
   addModalEvent() {
     this.botaoAbrir.addEventListener('click', this.eventToggleModal);
+    this.botaoFechar.addEventListener('click', this.eventToggleModal);
     this.containerModal.addEventListener('click', this.cliqueForaModal);
   }
-    
+
   init() {
     if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
       this.addModalEvent();
     }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ tabNav.init();
 const modal = new Modal('[data-modal="abrir"]', '[data-modal = "fechar"]', '[data-modal="container"]');
 modal.init();
 
+
 initToolTip();
 initDropDownMenu();
 initMenuMobile();
