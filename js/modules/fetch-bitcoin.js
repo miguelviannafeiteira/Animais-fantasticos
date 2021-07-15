e6bacfb5d15b7aa8f86d9847842b4f655595d43f
Miguel <miguelviannafeiteira@gmail.com>
2021-07-15 09:21:16 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,8 +1,8 @@
-export default function initFetchbitcoin() {
-  fetch('https://www.blockchain.com/ticker')
+export default function fetchbitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (1000/ bitcoin.BRL.sell).toFixed(4);// 1000 reais em bitcoin e só para aparcer 4 casas decimais
     }).catch((erro) => {
       console.log(Error(erro));
